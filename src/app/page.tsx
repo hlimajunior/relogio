76ad2d58cf96d0ab6e454a4706dd6a713a0fac26
Humberto Lima Júnior <hlimajunior@gmail.com>
2023-08-27 20:56:59 -0300

    Correção do mês

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Clock() {
   }, []);
 
   const day = time.getDate().toString().padStart(2, "0");
-  const month = time.getMonth().toString().padStart(2, "0");
+  const month = (time.getMonth()+1).toString().padStart(2, "0");
   const hours = time.getHours().toString();
   const minutes = time.getMinutes().toString().padStart(2, "0");
   const seconds = time.getSeconds().toString().padStart(2, "0");
