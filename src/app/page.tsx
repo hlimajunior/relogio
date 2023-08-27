533e37a835cc9383646e8c6b0ce177fa044df000
Humberto Lima Júnior <hlimajunior@gmail.com>
2023-08-27 20:42:43 -0300

    Limpando código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { format } from "date-fns";
 import { utcToZonedTime } from "date-fns-tz";
 
 export default function Clock() {
@@ -14,23 +13,20 @@ export default function Clock() {
     const interval = setInterval(() => {
       setTime(utcToZonedTime(new Date(), saoPauloTimeZone));
     }, 1000);
-
     return () => clearInterval(interval);
   }, []);
 
   const day = time.getDate().toString().padStart(2, "0");
   const month = time.getMonth().toString().padStart(2, "0");
-  const year = time.getFullYear().toString().padStart(2, "0");
   const hours = time.getHours().toString();
   const minutes = time.getMinutes().toString().padStart(2, "0");
   const seconds = time.getSeconds().toString().padStart(2, "0");
-  const formattedTime = format(time, "HH:mm:ss");
 
   return (
     <div className="h-screen flex flex-col justify-center items-center ">
       <span
-        className="flex flex-col justify-center items-center font-semibold font-sans text-[250px]
-       text-red-500"
+        className="flex flex-col justify-center items-center 
+            font-semibold font-sans text-[250px] text-red-500"
       >
         {hours}
         <br />
